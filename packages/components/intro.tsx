453d18e80863dcf6da795aec91ef4708493a2029
fazlerocks <fazle@hashnode.com>
2023-09-20 12:45:37 -0700

    Add confirmation message to newsletter component

--- packages/components/intro.tsx
+++ packages/components/intro.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 
 const Intro = () => {
-  const baseUrl = `${process.env.NEXT_PUBLIC_MODE === 'development' ? 'http://' : 'https://'}${process.env.NEXT_PUBLIC_BASE_URL}`;
+  const baseUrl = `${
+    process.env.NEXT_PUBLIC_MODE === "development" ? "http://" : "https://"
+  }${process.env.NEXT_PUBLIC_BASE_URL}`;
 
   return (
     <section className="grid grid-cols-1 gap-10 md:grid-cols-2">
@@ -30,7 +32,7 @@ const Intro = () => {
           <h3 className="font-semibold text-slate-700 dark:text-neutral-300">
             Subscribe to our blog updates
           </h3>
-          <div className="relative w-full p-2 bg-white rounded-full dark:bg-neutral-950">
+          {/* <div className="relative w-full p-2 bg-white rounded-full dark:bg-neutral-950">
             <input
               type="email"
               placeholder="john@doe.com"
@@ -39,6 +41,16 @@ const Intro = () => {
             <button className="absolute px-3 py-2 text-white rounded-full bg-primary-600 dark:bg-primary-500 top-3 right-3">
               Subscribe
             </button>
+          </div> */}
+          <div className="relative w-full p-2">
+            <p className="text-sm font-bold text-green-600 dark:text-green-500">
+              Almost there!
+            </p>
+            <p className="text-sm font-medium text-slate-600 dark:text-neutral-300">
+              Check your inbox for a confirmation email and click{" "}
+              <strong>"Confirm and Subscribe"</strong> to complete your
+              subscription. Thanks for joining us!
+            </p>
           </div>
         </div>
       </div>
