5ba7ab5fc11186f1ca210f778bd7ed6f5f98cbda
Sandeep Panda <sandeep@hashnode.com>
2023-10-02 11:37:15 -0400

    refactor slug catch-all route

--- packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
@@ -35,14 +35,8 @@ type Props = {
   publication: PublicationType;
 };
 
-export default function Post({ publication, post, page }: Props) {
-  if (!post && !page) {
-    return <ErrorPage statusCode={404} />;
-  }
-  const title = post ? post.title : page.title;
-  const highlightJsMonokaiTheme =
-    ".hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}";
-
+const Post = (post: PostType) => {
+  const title = post.title;
   const schema = {
     "@context": "https://schema.org/",
     "@type": "Blog",
@@ -92,17 +86,71 @@ export default function Post({ publication, post, page }: Props) {
       },
     ],
   };
+  const highlightJsMonokaiTheme =
+    ".hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}";
 
-  const tagsList = post && post.tags.map((tag) => (
-    <li key={tag.id}>
-      <a
-        href={`/tag/${tag.slug}`}
-        className="block px-2 py-1 font-medium border rounded-full dark:border-neutral-800 dark:hover:bg-neutral-800 md:px-4 hover:bg-slate-50"
-      >
-        #{tag.slug}
-      </a>
-    </li>
-  ));
+  const tagsList =
+    post &&
+    post.tags.map((tag) => (
+      <li key={tag.id}>
+        <a
+          href={`/tag/${tag.slug}`}
+          className="block px-2 py-1 font-medium border rounded-full dark:border-neutral-800 dark:hover:bg-neutral-800 md:px-4 hover:bg-slate-50"
+        >
+          #{tag.slug}
+        </a>
+      </li>
+    ));
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="canonical" href={post.url} />
+        <meta property="og:image" content={post.ogMetaData.image} />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+        <style
+          dangerouslySetInnerHTML={{ __html: highlightJsMonokaiTheme }}
+        ></style>
+      </Head>
+      <PostHeader
+        title={post.title}
+        coverImage={post.coverImage?.url}
+        date={post.publishedAt}
+        author={post.author}
+      />
+      {post.features.tableOfContents.isEnabled && <PostTOC />}
+      <PostBody contentMarkdown={post.content.markdown} />
+      <div className="w-full px-5 mx-auto md:max-w-screen-md text-slate-600 dark:text-neutral-300">
+        <ul className="flex flex-row flex-wrap items-center gap-2">
+          {tagsList}
+        </ul>
+      </div>
+      {/* <PostComments author={post.author} /> */}
+      <Subscribe />
+    </>
+  );
+};
+
+const Page = (page: PageType) => {
+  const title = page.title;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <PostBody contentMarkdown={page.content.markdown} />
+    </>
+  );
+};
+
+export default function PostOrPage({ publication, post, page }: Props) {
+  if (!post && !page) {
+    return <ErrorPage statusCode={404} />;
+  }
 
   return (
     <AppProvider publication={publication} post={post}>
@@ -110,37 +158,7 @@ export default function Post({ publication, post, page }: Props) {
         <Header />
         <Container className="pt-10">
           <article className="flex flex-col items-start gap-10 pb-10">
-            <Head>
-              <title>{title}</title>
-              {post ? (
-                <>
-                  <link rel="canonical" href={post.url} />
-                  <meta property="og:image" content={post.ogMetaData.image} />
-                  <script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-                  />
-                </>
-              ) : null}
-              <style
-                dangerouslySetInnerHTML={{ __html: highlightJsMonokaiTheme }}
-              ></style>
-            </Head>
-            {post && <PostHeader
-              title={post.title}
-              coverImage={post.coverImage?.url}
-              date={post.publishedAt}
-              author={post.author}
-            />}
-            {post && post.features.tableOfContents.isEnabled && <PostTOC />}
-            <PostBody contentMarkdown={post ? post.content.markdown : page.content.markdown} />
-            {post && <div className="w-full px-5 mx-auto md:max-w-screen-md text-slate-600 dark:text-neutral-300">
-              <ul className="flex flex-row flex-wrap items-center gap-2">
-                {tagsList}
-              </ul>
-            </div>}
-            {/* <PostComments author={post.author} /> */}
-            {post && <Subscribe />}
+            {post ? Post(post) : Page(page)}
           </article>
         </Container>
         <Footer />
