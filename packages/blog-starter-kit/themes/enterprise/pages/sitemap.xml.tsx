3323c36b07e0b67cf6d7e2b9c66b28c800e118bf
Sandeep Panda <sandeep@hashnode.com>
2023-10-03 13:25:30 -0400

    Add sitemap page

---
+++ packages/blog-starter-kit/themes/enterprise/pages/sitemap.xml.tsx
@@ -0,0 +1,31 @@
+import request from "graphql-request";
+import { SitemapDocument, SitemapQuery, SitemapQueryVariables } from "../generated/graphql";
+import getSitemap from "@starter-kit/utils/seo/sitemap";
+
+const GQL_ENDPOINT = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT;
+const Sitemap = () => null;
+
+export async function getServerSideProps(ctx: { req: any; res: any; query: any }) {
+  const { res } = ctx;
+  
+  const data = await request<
+  SitemapQuery,
+  SitemapQueryVariables
+  >(GQL_ENDPOINT, SitemapDocument, {
+    host: process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST,
+    postsCount: 50,
+    staticPagesCount: 50,
+  });
+
+  const publication = data.publication;
+  const xml = getSitemap(publication);
+
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+  res.setHeader('content-type', 'text/xml');
+  res.write(xml);
+  res.end();
+
+  return { props: {} };
+}
+
+export default Sitemap;
