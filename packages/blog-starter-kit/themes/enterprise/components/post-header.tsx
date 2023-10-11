e9a812045d0cc78b725d22975b950bb737757e44
Lakshya Thakur <lapstjup@gmail.com>
2023-10-11 14:23:05 +0530

    cover image for article is LCP

--- packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
@@ -2,9 +2,7 @@ import Avatar from "./avatar";
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import PostTitle from "./post-title";
-import {
-  User
-} from "../generated/graphql";
+import { User } from "../generated/graphql";
 import { resizeImage } from "@starter-kit/utils/image";
 
 type Props = {
@@ -19,12 +17,21 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
     <>
       <PostTitle>{title}</PostTitle>
       <div className="flex-row items-center justify-center hidden w-full gap-5 md:flex text-slate-700 dark:text-neutral-300">
-        <Avatar username={author.username} name={author.name} size={10} picture={author.profilePicture} />
+        <Avatar
+          username={author.username}
+          name={author.name}
+          size={10}
+          picture={author.profilePicture}
+        />
         <DateFormatter dateString={date} />
       </div>
       {coverImage && (
         <div className="w-full px-5 sm:mx-0">
-          <CoverImage title={title} src={resizeImage(coverImage, { w: 1600, h: 840, c: 'thumb' })} />
+          <CoverImage
+            title={title}
+            src={resizeImage(coverImage, { w: 1600, h: 840, c: "thumb" })}
+            priority={true}
+          />
         </div>
       )}
     </>
