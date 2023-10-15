ab53d65a338d98e65f4c103257af5b94b240c545
Sandeep Panda <sandeep@hashnode.com>
2023-10-15 14:44:53 -0400

    Add draft preview pages

--- packages/blog-starter-kit/themes/personal/pages/draft/[id].tsx
+++ packages/blog-starter-kit/themes/personal/pages/draft/[id].tsx
@@ -1,20 +1,21 @@
+import { resizeImage } from '@starter-kit/utils/image';
 import request from 'graphql-request';
 import ErrorPage from 'next/error';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Container } from '../../components/container';
 import { AppProvider } from '../../components/contexts/appContext';
+import { CoverImage } from '../../components/cover-image';
 import { Footer } from '../../components/footer';
-import { Header } from '../../components/header';
 import { Layout } from '../../components/layout';
 import { MarkdownToHtml } from '../../components/markdown-to-html';
-import { PostHeader } from '../../components/post-header';
+import { PersonalHeader } from '../../components/personal-theme-header';
 import {
-	Post,
-	Publication,
 	DraftByIdDocument,
 	DraftByIdQuery,
 	DraftByIdQueryVariables,
+	Post,
+	Publication,
 	PublicationByHostDocument,
 	PublicationByHostQuery,
 	PublicationByHostQueryVariables,
@@ -29,9 +30,16 @@ export default function Post({ publication, post }: Props) {
 	if (!post) {
 		return <ErrorPage statusCode={404} />;
 	}
-	const title = `${post.title} | Next.js Blog Example with Hashnode`;
 	const highlightJsMonokaiTheme =
 		'.hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}';
+
+	const coverImageSrc = !!post.coverImage?.url
+		? resizeImage(post.coverImage.url, {
+				w: 1600,
+				h: 840,
+				c: 'thumb',
+		  })
+		: undefined;
 
 	const tagsList = post.tags?.map((tag) => (
 		<li key={tag.id}>
@@ -45,29 +53,32 @@ export default function Post({ publication, post }: Props) {
 	));
 
 	return (
-		<AppProvider publication={publication}>
+		<AppProvider publication={publication} post={post}>
 			<Layout>
-				<Header />
-				<Container className="pt-10">
+				<Container className="mx-auto flex max-w-2xl flex-col items-stretch gap-10 px-5 py-10">
+					<PersonalHeader />
 					<article className="flex flex-col items-start gap-10 pb-10">
 						<Head>
-							<title>{title}</title>
-							<link rel="canonical" href={post.url} />
+							<title>{post.seo?.title || post.title}</title>
 							<style dangerouslySetInnerHTML={{ __html: highlightJsMonokaiTheme }}></style>
 						</Head>
-						<PostHeader
-							title={post.title}
-							coverImage={post.coverImage?.url}
-							date={post.publishedAt}
-							author={post.author}
-						/>
+						<h1 className="text-4xl leading-tight tracking-tight text-black dark:text-white">
+							{post.title}
+						</h1>
+						{!!coverImageSrc && (
+							<div className="w-full">
+								<CoverImage title={post.title} priority={true} src={coverImageSrc} />
+							</div>
+						)}
 						<MarkdownToHtml contentMarkdown={post.content.markdown} />
-						<div className="mx-auto w-full px-5 text-slate-600 dark:text-neutral-300 md:max-w-screen-md">
-							<ul className="flex flex-row flex-wrap items-center gap-2">{tagsList}</ul>
-						</div>
+						{(post.tags ?? []).length > 0 && (
+							<div className="mx-auto w-full text-slate-600 dark:text-neutral-300 md:max-w-screen-md">
+								<ul className="flex flex-row flex-wrap items-center gap-2">{tagsList}</ul>
+							</div>
+						)}
 					</article>
+					<Footer />
 				</Container>
-				<Footer />
 			</Layout>
 		</AppProvider>
 	);
