85b3d7d129235f961a72e16b2bb656c0e77c62b3
fazlerocks <fazle@hashnode.com>
2023-10-15 17:21:47 +0530

    Responsive fix for the footer

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -24,9 +24,9 @@ export const Footer = () => {
 						{publication.title}
 					</p>
 				)}
-				<div className="grid w-full grid-cols-5 gap-5">
-					<div className="col-span-3 grid grid-cols-4 gap-5">
-						<div className="col-span-1">
+				<div className="grid w-full grid-cols-3 gap-5 md:grid-cols-6 lg:grid-cols-5">
+					<div className="col-span-1 grid grid-cols-4 gap-5 md:col-span-4 lg:col-span-3">
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">
 								Stay in touch
 							</p>
@@ -53,7 +53,7 @@ export const Footer = () => {
 								</li>
 							</ul>
 						</div>
-						<div className="col-span-1">
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Resources</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
@@ -78,7 +78,7 @@ export const Footer = () => {
 								</li>
 							</ul>
 						</div>
-						<div className="col-span-1">
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Product</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
@@ -129,7 +129,7 @@ export const Footer = () => {
 							</ul>
 						</div>
 					</div>
-					<div className="col-span-2 flex flex-col items-end gap-5 text-slate-600 dark:text-neutral-300">
+					<div className="col-span-2 flex flex-col items-end gap-5 text-right text-slate-600 dark:text-neutral-300 md:text-left">
 						<SocialLinks />
 						<p>&copy; 2023 Company Inc.</p>
 						<p>
