579bc32cbf5a0302e081e36d4b3fc6d6098db4b2
Ayodele Samuel Adebayo <ayodelesamueladebayo143@gmail.com>
2023-10-21 23:26:49 +0100

    Add post read time in minute to post header and display author, date and read time on mobile

--- packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/post-header.tsx
@@ -3,6 +3,7 @@ import { User } from '../generated/graphql';
 import { Avatar } from './avatar';
 import { CoverImage } from './cover-image';
 import { DateFormatter } from './date-formatter';
+import { ReadTimeInMinutes } from './post-read-time-in-minutes';
 import { PostTitle } from './post-title';
 
 type Author = Pick<User, 'username' | 'name' | 'profilePicture'>;
@@ -12,20 +13,24 @@ type Props = {
 	coverImage: string | null | undefined;
 	date: string;
 	author: Author;
+	readTimeInMinutes: number;
 };
 
-export const PostHeader = ({ title, coverImage, date, author }: Props) => {
+export const PostHeader = ({ title, coverImage, date, author, readTimeInMinutes }: Props) => {
 	return (
 		<>
 			<PostTitle>{title}</PostTitle>
-			<div className="hidden w-full flex-row items-center justify-center gap-5 text-slate-700 dark:text-neutral-300 md:flex">
+			<div className="flex w-full flex-col items-center justify-center gap-5 text-slate-700 dark:text-neutral-300 md:flex-row md:gap-0">
 				<Avatar
 					username={author.username}
 					name={author.name}
 					size={8}
 					picture={author.profilePicture}
 				/>
-				<DateFormatter dateString={date} />
+				<section className="flex">
+					<DateFormatter dateString={date} />
+					<ReadTimeInMinutes readTimeInMinutes={readTimeInMinutes} />
+				</section>
 			</div>
 			{coverImage && (
 				<div className="w-full px-5 sm:mx-0">
