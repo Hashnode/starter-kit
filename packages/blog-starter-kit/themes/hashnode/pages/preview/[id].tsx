a460aeb2153d5f3fb5b00145c59df50d1c9cefec
Harsh <harsh@hashnode.com>
2023-11-01 19:25:52 +0530

    profile picture, content rendering and cover image issue fix

--- packages/blog-starter-kit/themes/hashnode/pages/preview/[id].tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/preview/[id].tsx
@@ -30,6 +30,7 @@ import { BookOpenSVG } from '../../components/icons/svgs';
 import getReadTime from '../../utils/getReadTime';
 import Autolinker from "../../utils/autolinker";
 import DraftFloatingMenu from '../../components/draft-floating-menu';
+import { markdownToHtml } from '@starter-kit/utils/renderer/markdownToHtml';
 
 type Props = {
 	draft: any; // TODO: to be fixed
@@ -38,7 +39,6 @@ type Props = {
 
 export default function Post({ publication, draft }: Props) {
 	const headerRef = useRef<HTMLElement | null>(null);
-	const [showCommentsSheet, setShowCommentsSheet] = useState(false);
 	if (!draft) {
 		return <ErrorPage statusCode={404} />;
 	}
@@ -47,8 +47,8 @@ export default function Post({ publication, draft }: Props) {
 		'.hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}';
 	const navPositionStyles = 'relative transform-none md:sticky md:top-0 md:left-0 md:backdrop-blur-lg';
 	const readTime = draft && getReadTime(draft.content.markdown);
-
-	const postContent = Autolinker.link(draft.content.markdown, {
+	const content = markdownToHtml(draft.content.markdown);
+	const postContent = Autolinker.link(content, {
 		twitter: true,
 		truncate: 45,
 		className: 'autolinkedURL',
@@ -105,13 +105,13 @@ export default function Post({ publication, draft }: Props) {
 										className="mb-0 block w-full"
 										placeholder="blur"
 										originalSrc={draft.coverImage.url}
-										src={resizeImage(draft.coverImage, {
+										src={resizeImage(draft.coverImage.url, {
 											w: 1600,
 											h: 840,
 											...(!draft.coverImage?.isPortrait ? { c: 'thumb' } : { fill: 'blur' }),
 										})}
 										blurDataURL={getBlurHash(
-											resizeImage(draft.coverImage, {
+											resizeImage(draft.coverImage.url, {
 											...blurImageDimensions,
 											...(!draft.coverImage?.isPortrait ? { c: 'thumb' } : { fill: 'blur' }),
 											}),
@@ -260,9 +260,6 @@ export async function getStaticProps({ params }: Params) {
 	
 	const publication = dataPublication.publication;
 	const draft = dataDraft.draft;
-	console.log("Params ID: ", params.id);
-	console.log("Publication: ", publication);
-	console.log("Post: ", draft);
 	return {
 		props: {
 			draft,
