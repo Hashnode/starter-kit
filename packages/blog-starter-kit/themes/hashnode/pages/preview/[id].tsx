21c52bcf5e218f2a484dc220f537a63af4324d1c
Sandeep Panda <sandeep@hashnode.com>
2023-11-08 14:26:29 -0800

    Regenerate lockfile

--- packages/blog-starter-kit/themes/hashnode/pages/preview/[id].tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/preview/[id].tsx
@@ -20,7 +20,7 @@ import {
 	PublicationFragment,
 } from '../../generated/graphql';
 import PublicationFooter from '../../components/publication-footer';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { twJoin } from 'tailwind-merge';
 import CustomImage from '../../components/custom-image';
 import { getBlurHash, imageReplacer, resizeImage } from '../../utils/image';
@@ -43,7 +43,7 @@ export default function Post({ publication, draft }: Props) {
 	if (!draft) {
 		return <ErrorPage statusCode={404} />;
 	}
-	const title = `${draft.title} | Next.js Blog Example with Hashnode`;
+	const title = `${draft.title} - Hashnode`;
 	const highlightJsMonokaiTheme =
 		'.hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}';
 	const navPositionStyles = 'relative transform-none md:sticky md:top-0 md:left-0 md:backdrop-blur-lg';
@@ -192,8 +192,6 @@ export default function Post({ publication, draft }: Props) {
 												__html: imageReplacer(postContent),
 												}}
 											/>
-
-											{/* {isPublicationPost && renderPinnedWidgets(props.widgets, 'bottom')} */}
 
 											<DraftFloatingMenu
 												draft={draft}
