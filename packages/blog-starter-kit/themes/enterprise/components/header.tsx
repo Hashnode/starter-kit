75b600666ea60ea2cc448640beba69cbea50df17
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-11-14 16:37:55 +0200

    Navbar mobile version

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -1,13 +1,6 @@
-import { Popover, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import {
-	ArrowPathIcon,
-	Bars3Icon,
-	ChartPieIcon,
-	CursorArrowRaysIcon,
-	FingerPrintIcon,
-	SquaresPlusIcon,
-} from '@heroicons/react/24/outline';
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
+import { ChevronDownIcon, XMarkIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 import { Fragment, useState } from 'react';
 
 import Link from 'next/link';
@@ -42,39 +35,34 @@ function hasUrl(
 	return !!navbarItem.url && navbarItem.url.length > 0;
 }
 
-const products = [
+const product1 = [
 	{
-		name: 'Analytics',
-		description: 'Get a better understanding of your traffic',
-		href: '#',
-		icon: ChartPieIcon,
+		name: 'MindsDB Pro',
+		description: 'Try our managed service',
+		href: 'https://cloud.mindsdb.com/',
+		Icon: <CloudNavBarSVG />,
 	},
 	{
-		name: 'Engagement',
-		description: 'Speak directly to your customers',
-		href: '#',
-		icon: CursorArrowRaysIcon,
-	},
-	{
-		name: 'Security',
-		description: 'Your customers’ data will be safe and secure',
-		href: '#',
-		icon: FingerPrintIcon,
-	},
-	{
-		name: 'Integrations',
-		description: 'Connect with third-party tools',
-		href: '#',
-		icon: SquaresPlusIcon,
-	},
-	{
-		name: 'Automations',
-		description: 'Build strategic funnels that will convert',
-		href: '#',
-		icon: ArrowPathIcon,
+		name: 'MindsDB Enterprise',
+		description: 'Contact sales',
+		href: 'https://mindsdb.com/book-a-demo',
+		Icon: <EnterpriseSVG />,
 	},
 ];
-
+const product2 = [
+	{
+		name: 'Github',
+		description: 'Visit our Repo',
+		href: 'https://github.com/mindsdb/mindsdb/',
+		Icon: <GithubSVG />,
+	},
+	{
+		name: 'Community',
+		description: ' Join our Slack',
+		href: 'https://mindsdb.com/community',
+		Icon: <CommunitySVG />,
+	},
+];
 const res1 = [
 	{
 		name: 'Support',
@@ -128,6 +116,41 @@ const res2 = [
 	},
 ];
 
+const company1 = [
+	{
+		name: 'About Us',
+		description: 'Our vision and team',
+		href: 'https://mindsdb.com/joincommunity',
+		Icon: <AboutUsNavSVG />,
+	},
+	{
+		name: 'Events',
+		description: 'Archive of past and upcoming events',
+		href: 'https://docs.mindsdb.com/',
+		Icon: <EventsSVG />,
+	},
+	{
+		name: 'Newsroom',
+		description: 'Read the latest news about MindsDB',
+		href: 'https://mindsdb.com/case-studies',
+		Icon: <NewsroomSVG />,
+	},
+];
+const company2 = [
+	{
+		name: 'Careers',
+		description: 'Learn about open roles at MindsDB',
+		href: 'https://mindsdb.com/machine-learning-use-cases',
+		Icon: <CareersSVG />,
+	},
+	{
+		name: 'Contacts',
+		description: 'Find the right channel to get in touch',
+		href: 'https://mindsdb.com/blog-categories/product-updates',
+		Icon: <ContactSVG />,
+	},
+];
+
 const NavItem = ({
 	title,
 	description,
@@ -172,34 +195,28 @@ const ProductPopover = () => {
 						<div className="w-full">
 							<p className="mb-4 border-b px-4 pb-3 text-black">CLOUD</p>
 
-							<NavItem
-								title="MindsDB Pro"
-								description="Try our managed service"
-								icon={<CloudNavBarSVG />}
-								url={'https://cloud.mindsdb.com/'}
-							/>
-							<NavItem
-								title="MindsDB Enterprise"
-								description="Contact sales"
-								icon={<EnterpriseSVG />}
-								url={'https://mindsdb.com/book-a-demo'}
-							/>
+							{product1.map((item) => (
+								<NavItem
+									key={item.name}
+									title={item.name}
+									description={item.description}
+									icon={item.Icon}
+									url={item.href}
+								/>
+							))}
 						</div>
 						<div className="w-full">
 							<p className="mb-4 border-b px-4 pb-3 text-black">OPEN-SOURCE</p>
 
-							<NavItem
-								title="Github"
-								description="Visit our Repo"
-								icon={<GithubSVG className="w-6" />}
-								url={'https://github.com/mindsdb/mindsdb/'}
-							/>
-							<NavItem
-								title="Community"
-								description="Join our Slack"
-								icon={<CommunitySVG />}
-								url={'https://mindsdb.com/community'}
-							/>
+							{product2.map((item) => (
+								<NavItem
+									key={item.name}
+									title={item.name}
+									description={item.description}
+									icon={item.Icon}
+									url={item.href}
+								/>
+							))}
 						</div>
 					</div>
 				</Popover.Panel>
@@ -255,41 +272,6 @@ const ResourcesPopover = () => {
 	);
 };
 const CompanyPopover = () => {
-	const res1 = [
-		{
-			name: 'About Us',
-			description: 'Our vision and team',
-			href: 'https://mindsdb.com/joincommunity',
-			Icon: <AboutUsNavSVG />,
-		},
-		{
-			name: 'Events',
-			description: 'Archive of past and upcoming events',
-			href: 'https://docs.mindsdb.com/',
-			Icon: <EventsSVG />,
-		},
-		{
-			name: 'Newsroom',
-			description: 'Read the latest news about MindsDB',
-			href: 'https://mindsdb.com/case-studies',
-			Icon: <NewsroomSVG />,
-		},
-	];
-	const res2 = [
-		{
-			name: 'Careers',
-			description: 'Learn about open roles at MindsDB',
-			href: 'https://mindsdb.com/machine-learning-use-cases',
-			Icon: <CareersSVG />,
-		},
-		{
-			name: 'Contacts',
-			description: 'Find the right channel to get in touch',
-			href: 'https://mindsdb.com/blog-categories/product-updates',
-			Icon: <ContactSVG />,
-		},
-	];
-
 	return (
 		<Popover className="relative">
 			<Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
@@ -309,7 +291,7 @@ const CompanyPopover = () => {
 				<Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-2xl overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
 					<div className="flex flex-row p-4 text-black ">
 						<div className="w-full">
-							{res1.map((item) => (
+							{company1.map((item) => (
 								<NavItem
 									key={item.name}
 									title={item.name}
@@ -320,7 +302,7 @@ const CompanyPopover = () => {
 							))}
 						</div>
 						<div className="w-full">
-							{res2.map((item) => (
+							{company2.map((item) => (
 								<NavItem
 									key={item.name}
 									title={item.name}
@@ -353,7 +335,7 @@ export const Header = () => {
 	return (
 		<header className="bg-primary-900 border-b px-10  text-white dark:border-neutral-800 ">
 			<div className="flex  h-20 ">
-				<div className="col-span-2 flex flex-1 flex-row items-center justify-start gap-2 lg:col-span-1">
+				<div className=" col-span-2 flex w-full flex-1 flex-row items-center justify-between gap-2 lg:col-span-1">
 					<h1>
 						<Link
 							href={'/'}
@@ -376,7 +358,7 @@ export const Header = () => {
 					{/* nav  */}
 
 					<nav
-						className=" flex max-w-7xl items-center justify-between p-6 lg:px-8"
+						className=" flex w-full items-center justify-between p-6 max-lg:justify-end lg:px-8"
 						aria-label="Global"
 					>
 						<div className="flex lg:hidden">
@@ -394,51 +376,48 @@ export const Header = () => {
 							<ResourcesPopover />
 							<CompanyPopover />
 						</Popover.Group>
+						<div className=" col-span-2  flex flex-row items-center justify-end gap-5 text-slate-300 max-lg:hidden  lg:col-span-3">
+							{/* <nav className="hidden lg:block">{navList}</nav> */}
+							<a href="https://github.com/mindsdb/mindsdb">
+								<img
+									height={60}
+									width={100}
+									alt="GitHub Repo stars"
+									src="https://img.shields.io/github/stars/mindsdb/mindsdb?style=social"
+								/>
+							</a>
+
+							{/* <Bars3Icon className=" h-10 w-6" aria-hidden="true" /> */}
+							<a href="https://mindsdb.com/joincommunity" className="flex flex-row gap-2">
+								<SlackWhiteNavSGV className="w-6" />
+								Join our Slack
+							</a>
+
+							<Button
+								href="https://github.com/mindsdb/mindsdb"
+								as="a"
+								type="outline"
+								label="LOG IN"
+								className="h-10 !text-sm"
+							/>
+							<Button
+								href="https://github.com/mindsdb/mindsdb"
+								as="a"
+								type="primary"
+								label="GET STARTED"
+								className="h-10 !text-sm"
+							/>
+						</div>
 					</nav>
 				</div>
-				<div className="col-span-2 flex flex-row items-center justify-end gap-5 text-slate-300 lg:col-span-3">
-					{/* <nav className="hidden lg:block">{navList}</nav> */}
-					<a href="https://github.com/mindsdb/mindsdb">
-						<img
-							height={60}
-							width={100}
-							alt="GitHub Repo stars"
-							src="https://img.shields.io/github/stars/mindsdb/mindsdb?style=social"
-						/>
-					</a>
 
-					{/* <Bars3Icon className=" h-10 w-6" aria-hidden="true" /> */}
-					<a href="https://mindsdb.com/joincommunity" className="flex flex-row gap-2">
-						<SlackWhiteNavSGV className="w-6" />
-						Join our Slack
-					</a>
-
-					<Button
-						href="https://github.com/mindsdb/mindsdb"
-						as="a"
-						type="outline"
-						label="LOG IN"
-						className="h-10 !text-sm"
-					/>
-					<Button
-						href="https://github.com/mindsdb/mindsdb"
-						as="a"
-						type="primary"
-						label="GET STARTED"
-						className="h-10 !text-sm"
-					/>
-				</div>
-				{/* <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+				<Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
 					<div className="fixed inset-0 z-10" />
 					<Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 						<div className="flex items-center justify-between">
 							<a href="#" className="-m-1.5 p-1.5">
-								<span className="sr-only">Your Company</span>
-								<img
-									className="h-8 w-auto"
-									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-									alt=""
-								/>
+								<span className="sr-only">Mindsdb</span>
+								<MindsdbLogoSVG className="h-8 w-auto" alt="mindsdb logo" />
 							</a>
 							<button
 								type="button"
@@ -463,102 +442,87 @@ export const Header = () => {
 													/>
 												</Disclosure.Button>
 												<Disclosure.Panel className="mt-2 space-y-2">
-													{[...products].map((item) => (
-														<Disclosure.Button
+													{[...product1, ...product2].map((item) => (
+														<NavItem
 															key={item.name}
-															as="a"
-															href={item.href}
-															className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-														>
-															{item.name}
-														</Disclosure.Button>
+															title={item.name}
+															description={item.description}
+															icon={item.Icon}
+															url={item.href}
+														/>
 													))}
 												</Disclosure.Panel>
 											</>
 										)}
 									</Disclosure>
-									<a
-										href="#"
-										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-									>
-										Features
-									</a>
-									<a
-										href="#"
-										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-									>
-										Marketplace
-									</a>
-									<a
-										href="#"
-										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-									>
-										Company
-									</a>
+									<Disclosure as="div" className="-mx-3">
+										{({ open }) => (
+											<>
+												<Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+													Resources
+													<ChevronDownIcon
+														className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+														aria-hidden="true"
+													/>
+												</Disclosure.Button>
+												<Disclosure.Panel className="mt-2 space-y-2">
+													{[...res1, ...res2].map((item) => (
+														<NavItem
+															key={item.name}
+															title={item.name}
+															description={item.description}
+															icon={item.Icon}
+															url={item.href}
+														/>
+													))}
+												</Disclosure.Panel>
+											</>
+										)}
+									</Disclosure>
+									<Disclosure as="div" className="-mx-3">
+										{({ open }) => (
+											<>
+												<Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+													Company
+													<ChevronDownIcon
+														className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+														aria-hidden="true"
+													/>
+												</Disclosure.Button>
+												<Disclosure.Panel className="mt-2 space-y-2">
+													{[...company1, ...company2].map((item) => (
+														<NavItem
+															key={item.name}
+															title={item.name}
+															description={item.description}
+															icon={item.Icon}
+															url={item.href}
+														/>
+													))}
+												</Disclosure.Panel>
+											</>
+										)}
+									</Disclosure>
 								</div>
 								<div className="py-6">
 									<a
-										href="#"
+										href="https://cloud.mindsdb.com/login"
 										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 									>
 										Log in
+									</a>
+									<a
+										href="https://github.com/mindsdb/mindsdb"
+										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									>
+										GET STARTED
 									</a>
 								</div>
 							</div>
 						</div>
 					</Dialog.Panel>
-				</Dialog> */}
+				</Dialog>
 			</div>
 		</header>
 	);
 };
-
-// const navList = (
-// 	<ul className="flex flex-row items-center gap-2 text-white">
-// 		{visibleItems.map((item) => (
-// 			<li key={item.url}>
-// 				<a
-// 					href={item.url}
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 					className="transition-200 block max-w-[200px] truncate text-ellipsis whitespace-nowrap rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
-// 				>
-// 					{item.label}
-// 				</a>
-// 			</li>
-// 		))}
-
-// 		{hiddenItems.length > 0 && (
-// 			<li>
-// 				<DropdownMenu.Root>
-// 					<DropdownMenu.Trigger asChild>
-// 						<button className="transition-200 block rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white">
-// 							More
-// 						</button>
-// 					</DropdownMenu.Trigger>
-
-// 					<DropdownMenu.Portal>
-// 						<DropdownMenu.Content
-// 							className="w-48 rounded border border-gray-300 bg-white text-neutral-950 shadow-md dark:border-neutral-800 dark:bg-neutral-900 dark:text-white"
-// 							align="end"
-// 							sideOffset={5}
-// 						>
-// 							{hiddenItems.map((item) => (
-// 								<DropdownMenu.Item asChild key={item.url}>
-// 									<a
-// 										href={item.url}
-// 										target="_blank"
-// 										rel="noopener noreferrer"
-// 										className="transition-200 block truncate p-2 transition-colors hover:bg-slate-100 hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
-// 									>
-// 										{item.label}
-// 									</a>
-// 								</DropdownMenu.Item>
-// 							))}
-// 						</DropdownMenu.Content>
-// 					</DropdownMenu.Portal>
-// 				</DropdownMenu.Root>
-// 			</li>
-// 		)}
-// 	</ul>
-// );
