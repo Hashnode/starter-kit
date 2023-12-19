fc2c0ce3dcc6c6ca22f78e61f3f375e7523de01e
Sandeep Panda <sandeep@hashnode.com>
2023-12-19 13:39:01 +0530

    Show example of how to add custom script

--- packages/blog-starter-kit/themes/enterprise/components/integrations.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/integrations.tsx
@@ -67,6 +67,13 @@ export function Integrations() {
 		});
 	}, []);
 
+	const googleTagManager = `
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MNN47J3');`;
+
 	return (
 		<>
 			{fbPixelID ? (
@@ -107,6 +114,11 @@ export function Integrations() {
 					src="https://plausible.io/js/plausible.js"
 				></script>
 			)}
+			{/* Add any custom integrations here */}
+			<script
+				type="text/javascript"
+				dangerouslySetInnerHTML={{ __html: googleTagManager }}
+			></script>
 		</>
 	);
 }
