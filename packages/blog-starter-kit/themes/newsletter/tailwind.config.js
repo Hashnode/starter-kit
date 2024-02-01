85bb202bdffebd9df438c38099686a0cab14e4c3
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-02-01 20:02:11 +0530

    style : Lora font family

--- packages/blog-starter-kit/themes/newsletter/tailwind.config.js
+++ packages/blog-starter-kit/themes/newsletter/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
 	darkMode: 'class',
 	theme: {
 		extend: {
+			fontFamily : {
+				'lora' : ['Lora', 'sans-serif']
+			},
 			colors: {
 				'accent-1': '#FAFAFA',
 				'accent-2': '#EAEAEA',
@@ -67,7 +70,11 @@ module.exports = {
 				sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
 				md: '0 8px 30px rgba(0, 0, 0, 0.12)',
 			},
+			
 		},
+		fontFamily : {
+			'lora' : ['Lora', 'sans-serif']
+		}
 	},
 	plugins: [require('@tailwindcss/typography')],
 };
