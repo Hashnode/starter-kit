6bf65649737b8fc9293d2cf3132ce70666a7e3fe
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-02-01 20:05:48 +0530

    feat : Hero Component

--- packages/blog-starter-kit/themes/newsletter/components/hero.tsx
+++ packages/blog-starter-kit/themes/newsletter/components/hero.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useAppContext } from './contexts/appContext'
-import { Navbar } from './navbar'
 
 const Hero = () => {
     const {publication} = useAppContext()
     const {author} = publication
   return (
-    <div className='min-h-[20vh] animate-up w-[90%] mx-auto flex flex-col items-center justify-center'>
-        <h1 className='text-5xl text-center  dark:text-white font-bold'>Welcome👋🏼 to <span className='text-primary-700'>{author.name}'s</span> blog posts!</h1>
-        <Navbar/>
+    <div className='min-h-[40vh] fade-in w-[90%] pt-10 mx-auto flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-5xl md:text-7xl text-center uppercase dark:text-white font-[600] flex flex-col gap-0'>{author.name} <span> newsletter</span></h1>
+        <p className='text-xl text-center text-gray-700 dark:text-gray-400'>{publication.descriptionSEO ? publication.descriptionSEO : "Welcome to My Blog Space: Dive into a world of thoughts and ideas."}</p>
+        
     </div>
   )
 }
