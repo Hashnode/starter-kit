cc233fa7cb42bed3833ec61baeafba3f9d481916
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-02-01 20:05:27 +0530

    feat : hero posts with theme and no cover

--- packages/blog-starter-kit/themes/newsletter/components/hero-post.tsx
+++ packages/blog-starter-kit/themes/newsletter/components/hero-post.tsx
@@ -17,26 +17,19 @@ export const HeroPost = ({ title, coverImage, date, excerpt, slug }: Props) => {
 	const postURL = `/${slug}`;
 
 	return (
-		<div className="grid grid-cols-1 self-stretch gap-5 outline outline-1 p-4 rounded-md outline-gray-200 transition-all duration-150">
-			<div className="col-span-1">
-				<CoverImage
-					title={title}
-					src={resizeImage(coverImage, { w: 1600, h: 840, c: 'thumb' }) || DEFAULT_COVER}
-					slug={slug}
-					priority={true}
-				/>
-			</div>
+		<div className="grid animate-up grid-cols-1 self-stretch gap-5   p-4">
+			
 			<div className="col-span-1 flex flex-col gap-2">
 				<h3 className="text-lg font-bold leading-snug text-slate-800 dark:text-neutral-50 lg:text-3xl">
 					<Link href={postURL}
 						
-						className="hover:text-primary-600 text-xl dark:hover:text-primary-500 leading-1 tracking-tight"
+						className=" text-xl hover:underline leading-1 tracking-tight"
 					>
 						{title}
 					</Link>
 				</h3>
 				
-					<p className="text-md leading-snug text-slate-500 dark:text-neutral-400">{excerpt}</p>
+					<p className="text-md leading-snug text-justify text-slate-500 dark:text-neutral-400">{excerpt}</p>
 				
 				<div className="text-sm font-semibold text-slate-500 dark:text-neutral-300">
 					<p >
