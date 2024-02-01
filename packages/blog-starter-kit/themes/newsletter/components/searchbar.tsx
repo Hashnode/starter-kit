9a5efdf761083368bb7e147bdea99733e483d6e2
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-02-01 20:07:56 +0530

    feat : search bar component with theme support

--- packages/blog-starter-kit/themes/newsletter/components/searchbar.tsx
+++ packages/blog-starter-kit/themes/newsletter/components/searchbar.tsx
@@ -1,4 +1,3 @@
-import { resizeImage } from '@starter-kit/utils/image';
 import request from 'graphql-request';
 import Link from 'next/link';
 import { KeyboardEventHandler, useEffect, useRef, useState } from 'react';
@@ -7,9 +6,7 @@ import {
 	SearchPostsOfPublicationQuery,
 	SearchPostsOfPublicationQueryVariables,
 } from '../generated/graphql';
-import { DEFAULT_COVER } from '../utils/const';
 import { useAppContext } from './contexts/appContext';
-import { CoverImage } from './cover-image';
 
 const GQL_ENDPOINT = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT;
 const NO_OF_SEARCH_RESULTS = 5;
@@ -76,28 +73,15 @@ export const Search = () => {
 			<Link
 				key={post.id}
 				href={postURL}
-				className="flex flex-row items-center border-t-[1px] border-primary-100 gap-5 px-4 py-2 bg-primary-50 transition-all duration-200 hover:bg-primary-100 focus:outline-1 dark:hover:bg-neutral-800"
+				className="flex flex-row items-center border-t-[1px] border-primary-100 gap-5 px-6 py-3 hover:bg-gray-100 transition-all duration-200 focus:outline-1 dark:hover:bg-neutral-800"
 			>
 				<div className="flex flex-col gap-1">
-					<strong className="text-base">{post.title}</strong>
-					<span className="text-slate-600 dark:text-neutral-300">
+					<strong className="text-base text-justify">{post.title}</strong>
+					<span className="text-slate-600 dark:text-neutral-300 text-justify">
 						{post.brief.length > 140 ? post.brief.substring(0, 140) + '…' : post.brief}
 					</span>
 				</div>
-				<div className="w-56">
-					<CoverImage
-						title={post.title}
-						src={resizeImage(
-							post.coverImage?.url,
-							{
-								w: 400,
-								h: 210,
-								c: 'thumb',
-							},
-							DEFAULT_COVER,
-						)}
-					/>
-				</div>
+				
 			</Link>
 		);
 	});
@@ -118,7 +102,7 @@ export const Search = () => {
 
 	return (
 		<div ref = {componentRef} className="animate-up relative w-full max-w-[560px] mx-auto">
-			<h3 className='text-center mb-4 text-3xl font-bold text-primary-950'>Have a specific blog in mind?</h3>
+			<h3 className='text-center mb-4 text-3xl font-bold text-primary-950'>Search my articles...</h3>
 			<input
 				
 				type="text"
@@ -127,31 +111,31 @@ export const Search = () => {
 				onKeyUp={escapeSearchOnESC}
 				onChange={updateSearchQuery}
 				placeholder="Search blog posts…"
-				className="w-full text-lg text-center rounded-md border border-slate-200 bg-slate-50 px-4 py-2 focus:bg-transparent dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-50 dark:placeholder:text-neutral-400 dark:hover:bg-neutral-950"
+				className="w-full text-lg rounded-md border border-slate-200  px-4 py-2 focus:bg-transparent dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-50 dark:placeholder:text-neutral-400 dark:hover:bg-neutral-950"
 			/>
 			{query && (
 				<>
 					{isSearching && (
-						<div className="top-100 absolute left-0 z-10 mt-1 flex w-full flex-col items-stretch overflow-hidden rounded-lg border bg-white p-1 text-left text-slate-900 shadow-2xl dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50">
+						<div className="top-100 absolute left-0 z-10 mt-1 flex w-full flex-col items-stretch overflow-hidden border bg-white p-1 text-left text-slate-900 shadow-2xl dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50">
 							<div className="flex animate-pulse flex-col gap-1 p-4">
-								<div className="h-8 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-2/3 rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-8 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-2/3 bg-slate-100 dark:bg-neutral-800"></div>
 							</div>
 							<div className="flex animate-pulse flex-col gap-1 p-4">
-								<div className="h-8 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-2/3 rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-8 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-2/3 bg-slate-100 dark:bg-neutral-800"></div>
 							</div>
 							<div className="flex animate-pulse flex-col gap-1 p-4">
-								<div className="h-8 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-full rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
-								<div className="h-4 w-2/3 rounded-lg bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-8 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-full bg-slate-100 dark:bg-neutral-800"></div>
+								<div className="h-4 w-2/3 bg-slate-100 dark:bg-neutral-800"></div>
 							</div>
 						</div>
 					)}
 					{searchResults.length > 0 && !isSearching && (
-						<div className="top-100 max-h-[400px] overflow-y-scroll absolute left-0 z-10 mt-1 flex w-full flex-col items-stretch overflow-hidden rounded-lg border bg-white p-1 text-left text-slate-900 shadow-2xl dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50">
+						<div className="top-100 max-h-[400px] overflow-y-scroll absolute left-0 z-10 mt-1 flex w-full flex-col items-stretch overflow-hidden border bg-white text-left text-slate-900 shadow-2xl dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50">
 							<h3 className="px-4 py-2 font-medium text-slate-500 dark:text-neutral-400">
 								Found {searchResults.length} results
 							</h3>
