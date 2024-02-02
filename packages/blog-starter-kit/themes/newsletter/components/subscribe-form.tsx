7562d0e46611c7f64dafb8cf45946d8bc8eaf89f
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-02-02 21:49:06 +0530

    style : better subscribe message

--- packages/blog-starter-kit/themes/newsletter/components/subscribe-form.tsx
+++ packages/blog-starter-kit/themes/newsletter/components/subscribe-form.tsx
@@ -1,5 +1,5 @@
 import request from 'graphql-request';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
 	SubscribeToNewsletterDocument,
 	SubscribeToNewsletterMutation,
@@ -7,6 +7,7 @@ import {
 	SubscribeToNewsletterPayload,
 } from '../generated/graphql';
 import { useAppContext } from './contexts/appContext';
+import GreenTickSVG from './icons/svgs/GreenTickSVG';
 
 const GQL_ENDPOINT = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT;
 
@@ -39,33 +40,43 @@ export const SubscribeForm = () => {
 			setRequestInProgress(false);
 		}
 	};
+
+	useEffect(() =>{
+		if(status === 'PENDING'){
+			setTimeout(() => {
+				setStatus(null)
+			} , 4000)
+		}
+	} , [status])
 	return (
 		<>
 			{!status && (
-				<div className="relative w-[80%]  md:outline outline-1 dark:outline-white  mx-auto rounded-md flex items-center flex-col md:flex-row gap-4 md:gap-0 bg-white dark:bg-gray-700">
+				<div className="relative w-[80%] fade-in  outline outline-1 dark:outline-black  mx-auto rounded-md flex items-center   bg-white dark:bg-gray-700 overflow-hidden">
 					<input
 						ref={inputRef}
 						type="email"
-						placeholder="john@doe.com"
-						className=" w-full py-2 px-4 text-base text-black outline-none dark:placeholder:text-gray-400 dark:text-neutral-50 focus: placeholder:text-gray-600 md:outline-none outline outline-1 outline-black"
+						placeholder="name@email.com"
+						className=" w-full py-2 px-4 text-base text-neutral-900 outline-none dark:placeholder:text-gray-400 dark:text-black focus: placeholder:text-gray-400 md:outline-none outline outline-1 outline-black "
 					/>
 					<button
 						disabled={requestInProgress}
 						onClick={subscribe}
-						className="bg-black h-full w-full md:w-fit dark:bg-gray-200 md:rounded-tr-md md:rounded-br-md dark:text-black px-3 py-2 text-white disabled:cursor-not-allowed disabled:opacity-80"
+						className="bg-black h-full   rounded-tr-md rounded-br-md px-3 py-2 text-white disabled:cursor-not-allowed disabled:opacity-80"
 					>
 						Subscribe
 					</button>
 				</div>
 			)}
 			{status === 'PENDING' && (
-				<div className="relative w-full p-2 text-center">
-					<p className="font-bold dark:text-white">Almost there!</p>
-					<p className="font-medium dark:text-white">
+				<div className="relative max-w-full flex flex-col fade-in items-center gap-2 w-[80%] p-2 text-center">
+					<span className=' text-green-700'><GreenTickSVG/></span>
+					<p className="font-bold text-xl text-green-700 dark:text-white">Almost there!</p>
+					<p className="font-medium text-green-700 dark:text-white">
 						Check your inbox for a confirmation email and click{' '}
 						<strong>&quot;Confirm and Subscribe&quot;</strong> to complete your subscription. Thanks
 						for joining us!
 					</p>
+					
 				</div>
 			)}
 		</>
