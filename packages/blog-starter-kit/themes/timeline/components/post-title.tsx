77a00472e68a929ed482f855d3b42da607e876a0
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-02-02 23:05:48 +0530

    changed font size for title on /slug route

--- packages/blog-starter-kit/themes/timeline/components/post-title.tsx
+++ packages/blog-starter-kit/themes/timeline/components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const PostTitle = ({ children }: Props) => {
 	return (
-		<div className="max-w-screen-lg px-5 mx-auto text-3xl prose text-center md:prose-xl dark:prose-invert prose-h1:text-center font-satoshiBold md:text-5xl">
+		<div className="max-w-screen-lg px-5 mx-auto text-3xl prose text-center md:prose-xl dark:prose-invert prose-h1:text-center font-satoshiBold md:text-4xl">
 			<h1 className="">{children}</h1>
 		</div>
 	);
