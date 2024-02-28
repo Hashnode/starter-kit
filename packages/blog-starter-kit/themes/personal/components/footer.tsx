bd8fc88bb3f969d989464c9b8060e062ffc4ff09
Haimantika Mitra <haimantikamitra@Haimantikas-MacBook-Pro.local>
2024-02-28 14:24:43 +0530

    Added footer

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -5,7 +5,38 @@ export const Footer = () => {
 
 	return (
 		<footer className="border-t pt-10 text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
-			&copy; {new Date().getFullYear()} {publication.title}
+			<footer id="footer" className="bg-black text-s text-white">
+			
+			<section
+			  className="mx-auto flex max-w-4xl flex-col p-4 sm:flex-row sm:justify-between"
+			>
+			  <nav>
+				<div className="flex justify-center gap-4">
+				  <a
+					href="https://twitter.com/HaimantikaM"
+					target="_blank"
+					className="text-current"
+					>Twitter</a>
+				  <a
+					href="https://newsletter.haimantika.com"
+					target="_blank"
+					className="text-current"
+					>Newsletter</a>
+				  <a
+					href="https://github.com/Haimantika"
+					target="_blank"
+					className="text-current"
+					>GitHub</a>
+				  <a
+					href="https://www.linkedin.com/in/haimantika-mitra/"
+					target="_blank"
+					className="text-current"
+					>LinkedIn</a>
+				</div>
+			  </nav>
+			</section>
+		  </footer>
+	
 		</footer>
 	);
 };
