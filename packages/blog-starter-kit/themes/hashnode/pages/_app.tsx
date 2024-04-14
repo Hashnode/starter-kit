f41b090d2e5486bc22bd7ad332eeb9a68adf75cd
Advaith Krishna A <76763029+AdvaithKrishnaA@users.noreply.github.com>
2024-04-14 12:43:53 +0530

    Update _app.tsx

--- packages/blog-starter-kit/themes/hashnode/pages/_app.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_app.tsx
@@ -7,6 +7,7 @@ import { GlobalFontVariables } from '../components/fonts';
 import { getUrqlClientConfig } from '../lib/api/client';
 
 import { Fragment } from 'react';
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -14,8 +15,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 			 <GlobalFontVariables />
 			 <Component {...pageProps} />
 		</Fragment>
+    <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
 	);
 }
+
+import { AppProps } from 'next/app';
+import '../styles/index.css';
 
 // `withUrqlClient` HOC provides the `urqlClient` prop and takes care of restoring cache from urqlState
 // this will provide ssr cache to the provider and enable to use `useQuery` hook on the client side
