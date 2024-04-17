b9182f7d6542d63ec383f149983399ae897c20a1
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-04-17 22:13:10 +0100

    feat: add read time and reaction count to slug page

--- packages/blog-starter-kit/themes/personal/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/personal/pages/[slug].tsx
@@ -128,11 +128,15 @@ const Post = ({ publication, post }: PostProps) => {
 				/>
 				<style dangerouslySetInnerHTML={{ __html: highlightJsMonokaiTheme }}></style>
 			</Head>
-			<h1 className="text-4xl leading-tight tracking-tight text-black dark:text-white">
+			<h1 className="text-4xl font-bold leading-tight tracking-tight text-black dark:text-white">
 				{post.title}
 			</h1>
-			<div className="text-neutral-600 dark:text-neutral-400">
+			<div className="flex tracking-tight gap-2 text-neutral-600 dark:text-neutral-400">
 				<DateFormatter dateString={post.publishedAt} />
+				{'•'}
+				<span>{post.readTimeInMinutes} min read</span>
+				{'•'}
+        <span>{post.reactionCount} likes</span>
 			</div>
 			{!!coverImageSrc && (
 				<div className="w-full">
