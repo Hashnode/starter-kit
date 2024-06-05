ed686e1c2958b5423539828a1d78e1877625a02e
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-06-05 22:06:09 +0530

    Update layout.tsx

--- packages/blog-starter-kit/themes/personal/components/layout.tsx
+++ packages/blog-starter-kit/themes/personal/components/layout.tsx
@@ -12,7 +12,7 @@ export const Layout = ({ children }: Props) => {
 		<>
 			<Meta />
 			<Scripts />
-			<div className="min-h-screen bg-white dark:bg-neutral-950">
+			<div className="min-h-screen bg-sky-100 dark:bg-neutral-950">
 				<main>{children}</main>
 			</div>
 			<Analytics />
