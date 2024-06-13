281e62c65729d61ebd17f3fc5d3f878f341dfe51
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-13 17:11:11 +0300

    hover underline deleted

--- packages/blog-starter-kit/themes/enterprise/components/secondary-post.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/secondary-post.tsx
@@ -28,7 +28,7 @@ export const SecondaryPost = ({ title, coverImage, date, excerpt, slug }: Props)
 				<h1 className="text-lg font-semibold leading-tight text-slate-800 dark:text-neutral-50">
 					<Link
 						href={postURL}
-						className="hover:text-primary-600 dark:hover:text-primary-500 hover:underline"
+						className="dark:hover:text-primary-500"
 					>
 						{title}
 					</Link>
