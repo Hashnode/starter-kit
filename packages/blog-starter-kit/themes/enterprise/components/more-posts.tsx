8786aca9f02a84204b5afc963e42f9b3fa1f6156
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-14 16:11:41 +0300

    up

--- packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
@@ -10,9 +10,9 @@ export const MorePosts = ({ posts, context }: Props) => {
 	return (
 		<section className="mb-10 flex flex-col items-start gap-10">
 			{context === 'home' && (
-				<h2 className="text-xl font-bold leading-tight tracking-tight text-slate-900 dark:text-neutral-50 lg:text-3xl">
-					More Posts
-				</h2>
+				<div className="text-center">
+				<img className="block w-40" src="https://9kelt5xnesj2nkgz.public.blob.vercel-storage.com/underline-G32Fu5noQ2QvXU7bIiaq9zclJsLPcm.png" />
+			  </div>
 			)}
 			<div className="grid items-start gap-5 md:grid-cols-2 xl:grid-cols-3">
 				{posts.map((post) => (
