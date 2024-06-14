68f4fa8a2509a4db7898b5dce918fb3530cca6fb
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-14 13:31:54 +0300

    updated progressbar

--- packages/blog-starter-kit/themes/enterprise/components/ProgressBar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/ProgressBar.tsx
@@ -20,13 +20,15 @@ const ProgressBar = () => {
   }, []);
 
   return (
-    <div className="fixed right-0 top-0 h-full w-2 bg-gray-200">
-      <div
-        className="bg-blue-500 h-full"
-        style={{ height: `${scrollPercentage}%` }}
-      ></div>
+    <div className="fixed right-0 top-0 h-full w-12 bg-gray-200 flex flex-col items-center justify-center">
       <div className="fixed right-4 top-2 text-black bg-white rounded px-2 py-1">
         {scrollPercentage.toFixed(0)}%
+      </div>
+      <div className="h-full w-2 bg-gray-200">
+        <div
+          className="bg-blue-500"
+          style={{ height: `${scrollPercentage}%`, width: '100%' }}
+        ></div>
       </div>
     </div>
   );
