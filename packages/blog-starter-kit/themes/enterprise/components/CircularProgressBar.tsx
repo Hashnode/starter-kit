6f35af224836e3c87a50d6c54179297c8528c8a9
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-14 15:12:16 +0300

    update

--- packages/blog-starter-kit/themes/enterprise/components/CircularProgressBar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/CircularProgressBar.tsx
@@ -42,14 +42,34 @@ const CircularProgressBar = () => {
           fill="none"
           strokeWidth="2"
           strokeDasharray={`${scrollPercentage}, 100`}
+          strokeDashoffset="25"
         />
       </svg>
-      <a href="#head" id="cta" onClick={scrollToTop} className="absolute flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg">
+      <a
+        href="#head"
+        id="cta"
+        onClick={scrollToTop}
+        className="absolute flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg"
+      >
         <span className="sr-only">Yukarı</span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 15.75 7.5-7.5 7.5 7.5"
+          />
         </svg>
       </a>
+      <div className="absolute top-1/2 transform -translate-y-1/2 text-sm text-black font-bold">
+        {Math.round(scrollPercentage)}%
+      </div>
     </div>
   );
 };
