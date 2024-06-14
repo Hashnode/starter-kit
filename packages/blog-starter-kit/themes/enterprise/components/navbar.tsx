61e974519ede47f852d9740904d42575209f6e1e
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-14 10:58:59 +0300

    update

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -4,6 +4,11 @@ export const Navbar = () => {
 	return (
 		<div className="grid grid-cols-1 items-center gap-5 pt-5 text-sm md:grid-cols-2">
 			<Search />
+            <div class="text-center">
+    
+    <h1 class="text-5xl text-gray-900 font-semibold mt-2 mb-5">Temizmama Blog</h1>
+    <p class="text-purple-500 text-lg max-w-xl mx-auto">Sevimli dostlarımız için en taze mamayı sunan Temizmama aracılığıyla kedi &amp; köpek bakımı ile ilgili bilinmesi gerekenlerin hepsi bu sayfada!</p>
+  </div>
 		</div>
 	);
 };
