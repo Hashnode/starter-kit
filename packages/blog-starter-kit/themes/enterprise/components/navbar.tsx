fe2e6715bee92e3e7e4d9d816c0c71e5538ea018
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-21 20:49:46 +0300

    fix

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image'
-
-
 
 export const Navbar = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
--- packages/blog-starter-kit/themes/enterprise/pages/iletisim.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/iletisim.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 
-
 const iletisim: React.FC = () => {
   return (
     <div className="iletisim">
