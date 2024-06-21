281608548953f96b3e7e050863202bac6da6d365
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-21 15:43:23 +0300

    fix

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
             <div className="hidden md:flex justify-end w-full">
               <ul className="flex items-center space-x-8">
                 <li style={{ opacity: 1, transform: "none" }}>
-                  <a className="text-gray-800 hover:text-gray-700/75 cursor-pointer" onClick={}>
+                  <a className="text-gray-800 hover:text-gray-700/75 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="navigation-search__icon">
                       <circle cx="11" cy="11" r="8"></circle>
                       <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
