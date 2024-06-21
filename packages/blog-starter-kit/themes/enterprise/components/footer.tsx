195e535f51a609e2cbade3f4c3313809f1d53725
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-21 17:14:48 +0300

    fix

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -121,7 +121,7 @@ return (
       </p>
     </div>
   </Container>
-  <a href="#head" id="cta">
+  <a href="#__next" id="cta">
   <span className="sr-only">Yukarı</span>
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
