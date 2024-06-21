fcb831fb04d35487f2becb6d979d1497eae06d29
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-21 17:35:10 +0300

    fix

---
+++ packages/blog-starter-kit/themes/enterprise/pages/iletisim.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const iletisim: React.FC = () => {
+  return (
+    <div className="iletisim">
+      <header className="iletisim-header">
+        <h1>In development!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default iletisim;
