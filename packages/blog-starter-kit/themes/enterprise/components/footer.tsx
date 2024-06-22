4f7f91da48c206e72ba50bd93eb494845dbc4080
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-22 16:07:23 +0300

    fix

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -121,13 +121,6 @@ return (
       </p>
     </div>
   </Container>
-  <a href="#__next" id="cta">
-  <span className="sr-only">Yukarı</span>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-  </svg>
-</a>
-
 </footer>
 );
 };
