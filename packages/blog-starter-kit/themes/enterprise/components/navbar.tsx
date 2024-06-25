ea79f5212e0e0b160b1138d2566836633a988a5f
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-25 10:25:50 +0300

    navbar mobil görünüm eklendi

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -2,91 +2,107 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 export const Navbar = () => {
-  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const handleSearchOpen = () => setIsSearchVisible(true);
-  const handleSearchClose = () => setIsSearchVisible(false);
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
   return (
-    <>
-      <nav className="absoulute top-0 w-full z-50 sm:p-6 p-3 animate-onload" style={{ opacity: 1, transform: "none" }}>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-white/10 backdrop-blur-md shadow-md py-4 rounded-xl select-none" style={{ opacity: 1, transform: "none", background: "hsl(30.98deg 100% 39.84% / 22%)" }}>
-          <div className="flex items-center justify-between h-10">
-            <div className="flex items-center">
+    <nav className=" top-0 w-full z-50 sm:p-6 p-3 animate-onload" style={{ opacity: 1, transform: "none" }}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-white/10 backdrop-blur-md shadow-md py-4 rounded-xl select-none" style={{ opacity: 1, transform: "none", background: "hsl(30.98deg 100% 39.84% / 22%)" }}>
+        <div className="flex items-center justify-between h-10">
+          <div className="flex items-center">
             <Link href="./">
-    <div style={{ position: 'relative', width: '100px', height: '53px', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', zoom: '1.6'}}>
-      <Image
-        src="https://9kelt5xnesj2nkgz.public.blob.vercel-storage.com/file-eYpF3jWI7j8924LUC1AR51hcMjnVNp.png"
-        alt="Ana Sayfa"
-        fill 
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
-        style={{ objectFit: 'contain' }} 
-      />
-    </div>
-  </Link>
-            </div>
-            <div className="hidden md:flex justify-end w-full">
-              <ul className="flex items-center space-x-8">
-                {/* <li style={{ opacity: 1, transform: "none" }}>
-                  <a className="text-gray-800 hover:text-gray-700/75 cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="navigation-search__icon">
-                      <circle cx="11" cy="11" r="8"></circle>
-                      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                  </a>
-                </li>
-                */}
-                <li style={{ opacity: 1, transform: "none" }}>
-                  <Link
-                    href={'./'}
-                    aria-label={`Ana Sayfa`}
-                    className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
-                    style={{ fontFamily: 'PinkChicken' }}
-                  >
-                    <span>
-                      Ana Sayfa
-                    </span>
-                  </Link>
-                </li>
-                <li style={{ opacity: 1, transform: "none" }}>
-                  <Link
-                    href={'./kurumsal'}
-                    aria-label={`kurumsal`}
-                    className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
-                    style={{ fontFamily: 'PinkChicken' }}
-                  >
-                    <span>
-                      Kurumsal
-                    </span>
-                  </Link>
-                </li>
-                <li style={{ opacity: 1, transform: "none" }}>
-                  <Link
-                    href={'./iletisim'}
-                    aria-label={`İletişim`}
-                    className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
-                    style={{ fontFamily: 'PinkChicken' }}
-                  >
-                    <span>
-                      İletişim
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="hidden md:block" style={{ opacity: 1, transform: "none" }}></div>
-            <div className="md:hidden flex items-center">
-              <button className="text-white" >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
-              </button>
-            </div>
+              <div style={{ position: 'relative', width: '100px', height: '53px', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', zoom: '1.6'}}>
+                <Image
+                  src="https://9kelt5xnesj2nkgz.public.blob.vercel-storage.com/file-eYpF3jWI7j8924LUC1AR51hcMjnVNp.png"
+                  alt="Ana Sayfa"
+                  fill 
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
+                  style={{ objectFit: 'contain' }} 
+                />
+              </div>
+            </Link>
+          </div>
+          <div className="hidden md:flex justify-end w-full">
+            <ul className="flex items-center space-x-8">
+              <li style={{ opacity: 1, transform: "none" }}>
+                <Link
+                  href='./'
+                  aria-label="Ana Sayfa"
+                  className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                >
+                  <span>Ana Sayfa</span>
+                </Link>
+              </li>
+              <li style={{ opacity: 1, transform: "none" }}>
+                <Link
+                  href='./kurumsal'
+                  aria-label="Kurumsal"
+                  className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                >
+                  <span>Kurumsal</span>
+                </Link>
+              </li>
+              <li style={{ opacity: 1, transform: "none" }}>
+                <Link
+                  href='./iletisim'
+                  aria-label="İletişim"
+                  className="text-gray-800 transition hover:text-gray-700/75 cursor-pointer font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                >
+                  <span>İletişim</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="md:hidden flex items-center">
+            <button className="text-gray-800" onClick={toggleMobileMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
           </div>
         </div>
-      </nav>
-      </>
+        {isMobileMenuOpen && (
+          <div className="md:hidden absolute top-full left-0 right-0 bg-white/10 backdrop-blur-md shadow-md rounded-b-xl mt-2 py-2 px-4" style={{ backgroundColor: 'hsl(26.84deg 63.33% 88.24%)' }}>
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="./"
+                  className="block text-gray-800 hover:text-gray-700/75 font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                  onClick={toggleMobileMenu}
+                >
+                  Ana Sayfa
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="./kurumsal"
+                  className="block text-gray-800 hover:text-gray-700/75 font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                  onClick={toggleMobileMenu}
+                >
+                  Kurumsal
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="./iletisim"
+                  className="block text-gray-800 hover:text-gray-700/75 font-bold"
+                  style={{ fontFamily: 'PinkChicken' }}
+                  onClick={toggleMobileMenu}
+                >
+                  İletişim
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
