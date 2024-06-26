af47c096e08e8bca04343d2d9573dbe2ea7c622b
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-26 17:38:32 +0300

    .

--- packages/blog-starter-kit/themes/enterprise/components/Contact.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/Contact.tsx
@@ -120,6 +120,12 @@ const ContactForm: React.FC<ContactProps> = ({ publication }) => {
 
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
+  const validateName = (name: string): boolean => {
+    const nameParts = name.split(/\s+/).filter(Boolean); // Split and filter out extra spaces
+    const trimmedName = nameParts.join(' '); // Join back with a single space
+    return /^[a-zA-ZığüşöçİĞÜŞÖÇ\s]+$/.test(trimmedName) && nameParts.length >= 2 && nameParts.length <= 3;
+  };
+  
   const validateForm = useCallback(debounce((data: FormData) => {
     const newMessageCharCount = countNonWhitespaceChars(data.message);
 
@@ -401,7 +407,7 @@ const validateName = (name: string): boolean => {
                 disabled={isButtonDisabled || !isMessageValid}
               >
                 {isButtonDisabled 
-                  ? 'Gönder' 
+                  ? 'Formu eksiksiz doldur' 
                   : isFormSubmitted
                     ? 'Gönderildi!'
                     : <span className="flex items-center justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 mr-2"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>Gönder</span>
