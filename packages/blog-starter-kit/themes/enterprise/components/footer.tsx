fb31e70b96b4523c7bab72b8b9b042b4359ee0a2
challengingfuture <challengingfuture@gmail.com>
2024-07-02 09:22:00 +0000

    . title eklendi footer ikonlara ve bağlantılara

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -73,6 +73,7 @@ return (
           className="text-gray-700 transition hover:text-gray-700/75"
           style={{ fontFamily: 'PinkChicken' }}
           rel="canonical"
+          title="Ana Sayfa"
         >
 					<span>
 						Ana Sayfa
@@ -81,19 +82,19 @@ return (
         </li>
 
         <li>
-          <a className="text-gray-700 transition hover:text-gray-700/75" href="https://www.temizmama.com" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Online Satış </a>
+          <a className="text-gray-700 transition hover:text-gray-700/75" title="Online Satış" href="https://www.temizmama.com" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Online Satış </a>
         </li>
 
         <li>
-          <a className="text-gray-700 transition hover:text-gray-700/75" href="https://www.temizmama.com/barinaklara-bagis" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Barınak Bağışı </a>
+          <a className="text-gray-700 transition hover:text-gray-700/75" title="Barınaklara bağış" href="https://www.temizmama.com/barinaklara-bagis" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Barınak Bağışı </a>
         </li>
 
         <li>
-          <a className="text-gray-700 transition hover:text-gray-700/75" href="https://www.temizmama.com/aydinlatma-metni" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Aydınlatma Metni </a>
+          <a className="text-gray-700 transition hover:text-gray-700/75" title="Aydınlatma Metni" href="https://www.temizmama.com/aydinlatma-metni" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Aydınlatma Metni </a>
         </li>
 
         <li>
-          <a className="text-gray-700 transition hover:text-gray-700/75" href="https://www.temizmama.com/hakkimizda" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Hakkımızda </a>
+          <a className="text-gray-700 transition hover:text-gray-700/75" title="Hakkımızda" href="https://www.temizmama.com/hakkimizda" target="_blank" style={{ fontFamily: 'PinkChicken'}}> Hakkımızda </a>
         </li>
 
         <li>
@@ -110,7 +111,7 @@ return (
 
       <ul className="mt-12 flex justify-center gap-6 md:gap-8 items-center">
         <li>
-          <a href="https://www.facebook.com/temizmama/" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+          <a href="https://www.facebook.com/temizmama/" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75" title="Facebook">
             <span className="sr-only">Facebook</span>
             <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd"
@@ -121,8 +122,8 @@ return (
         </li>
 
         <li>
-          <a href="https://www.instagram.com/temizmamacom" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
-            <span className="sr-only">Instagram</span>
+          <a href="https://www.instagram.com/temizmamacom" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75" title="İnstagram">
+            <span className="sr-only">İnstagram</span>
             <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd"
                 d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
@@ -132,7 +133,7 @@ return (
         </li>
 
         <li>
-          <a href="https://x.com/temizmamacom" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+          <a href="https://x.com/temizmamacom" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75" title="X (Twitter)">
             <span className="sr-only">X</span>
             <svg   className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd"
@@ -143,7 +144,7 @@ return (
         </li>
 
         <li>
-          <a href="https://www.youtube.com/@temizmama" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+          <a href="https://www.youtube.com/@temizmama" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75" title="Youtube">
             <span className="sr-only">Youtube</span>
             <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd" 
