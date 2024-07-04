ccf6aa1df8fd8ec843e4d9175d452f82d24c77ae
challengingfuture <challengingfuture@gmail.com>
2024-07-04 12:28:10 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/layout.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/layout.tsx
@@ -1,25 +1,37 @@
+// app/layout.tsx
+import { headers } from 'next/headers';
 import { Analytics } from './analytics';
 import { Integrations } from './integrations';
 import { Meta } from './meta';
 import { Scripts } from './scripts';
 import OfflineNotification from './OfflineNotification';
 
-
 type Props = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
-export const Layout = ({ children }: Props) => {
-	return (
-		<>
-			<Meta />
-			<Scripts />
-			<div className="min-h-screen bg-white dark:bg-neutral-950">
-				<main>{children}</main>
-				<OfflineNotification />
-			</div>
-			<Analytics />
-			<Integrations />
-		</>
-	);
-};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const policy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com ${process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT};
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    font-src 'self' data:;
+    connect-src *;
+  `;
+
+  headers().set('Content-Security-Policy', policy);
+
+  return (
+	<>
+        <Meta /> 
+        <Scripts />
+        <div className="min-h-screen bg-white dark:bg-neutral-950">
+          <main>{children}</main>
+          <OfflineNotification />
+        </div>
+        <Analytics />
+        <Integrations />
+	</>
+  );
+}
