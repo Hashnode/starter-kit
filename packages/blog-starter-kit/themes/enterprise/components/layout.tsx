4ec23895fe7d832c2b4ac4fa25f344f36ece8292
challengingfuture <challengingfuture@gmail.com>
2024-07-04 12:32:27 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/layout.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from './analytics';
-import { headers } from 'next/headers';
 import { Integrations } from './integrations';
 import { Meta } from './meta';
 import { Scripts } from './scripts';
@@ -9,16 +8,7 @@ import OfflineNotification from './OfflineNotification';
 type Props = {
 	children: React.ReactNode;
 };
-const policy = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com ${process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT};
-    style-src 'self' 'unsafe-inline';
-    img-src * blob: data:;
-    font-src 'self' data:;
-    connect-src *;
-  `;
 
-  headers().set('Content-Security-Policy', policy);
 export const Layout = ({ children }: Props) => {
 	return (
 		<>
