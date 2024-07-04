53b4fde0a0f0b79fbe3d1ccb95785235d3168d85
challengingfuture <challengingfuture@gmail.com>
2024-07-04 11:56:30 +0000

    hashnode ve personel temaları silindi

--- packages/blog-starter-kit/themes/.hashnode/process-env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-	interface ProcessEnv {
-		[key: string]: string | undefined;
-		NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT: string;
-		NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST: string;
-		// add more environment variables and their types here
-	}
-}
--- packages/blog-starter-kit/themes/personal/utils/const/index.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_AVATAR =
-	'https://cdn.hashnode.com/res/hashnode/image/upload/v1659089761812/fsOct5gl6.png';
