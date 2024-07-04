8940dcb4997b0f55cbdcea8d38d450d3c23b48ed
challengingfuture <challengingfuture@gmail.com>
2024-07-04 11:00:38 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/.eslintrc.js
+++ packages/blog-starter-kit/themes/enterprise/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
 	root: true,
 	extends: ['@starter-kit/eslint-config-custom'],
-};
+	parserOptions: {
+	  ecmaVersion: 2020,
+	  sourceType: 'module',
+	  ecmaFeatures: {
+		jsx: true,
+	  },
+	},
+  };
