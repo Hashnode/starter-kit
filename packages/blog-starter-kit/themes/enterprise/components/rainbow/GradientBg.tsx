9d571bdcc66b4570b959092996dc0e367f848c53
challengingfuture <challengingfuture@gmail.com>
2024-07-05 08:52:28 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
@@ -1,19 +1,24 @@
-import { useEffect } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import styles from "./GradientBg.module.css";
 
-const GradientBg = () => {
-  useEffect(() => {
-    const interBubble = document.querySelector(".interactive") as HTMLElement;
+const GradientBg: React.FC = () => {
+  const interBubbleRef = useRef<HTMLDivElement>(null);
+  const animationRef = useRef<number>();
+
+  const move = useCallback(() => {
+    const interBubble = interBubbleRef.current;
+    if (!interBubble) return;
+
     let curX = 0;
     let curY = 0;
     let tgX = 0;
     let tgY = 0;
 
-    const move = () => {
+    const animate = () => {
       curX += (tgX - curX) / 20;
       curY += (tgY - curY) / 20;
       interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      requestAnimationFrame(move);
+      animationRef.current = requestAnimationFrame(animate);
     };
 
     const handleMouseMove = (event: MouseEvent) => {
@@ -22,40 +27,48 @@ const GradientBg = () => {
     };
 
     window.addEventListener("mousemove", handleMouseMove);
-    move();
+    animate();
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      if (animationRef.current) {
+        cancelAnimationFrame(animationRef.current);
+      }
     };
   }, []);
 
+  useEffect(() => {
+    const cleanup = move();
+    return cleanup;
+  }, [move]);
+
   return (
-    <div className="gradient-bg" style={{ position: "fixed", zIndex: 0, opacity: 0.2 }}>
-      <svg viewBox="0 0 100vw 100vw" xmlns="http://www.w3.org/2000/svg" className="noiseBg">
+    <div className={styles.gradientBg}>
+      <svg className={styles.noiseBg} xmlns="http://www.w3.org/2000/svg">
         <filter id="noiseFilterBg">
-          <feTurbulence type="fractalNoise" baseFrequency="0.6" stitchTiles="stitch"></feTurbulence>
+          <feTurbulence type="fractalNoise" baseFrequency="0.6" stitchTiles="stitch" />
         </filter>
-        <rect width="100%" height="100%" preserveAspectRatio="xMidYMid meet" filter="url(#noiseFilterBg)"></rect>
+        <rect width="100%" height="100%" filter="url(#noiseFilterBg)" />
       </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" className="svgBlur">
+      <svg className={styles.svgBlur} xmlns="http://www.w3.org/2000/svg">
         <defs>
           <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo"></feColorMatrix>
-            <feBlend in="SourceGraphic" in2="goo"></feBlend>
+            <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -10" result="goo" />
+            <feBlend in="SourceGraphic" in2="goo" />
           </filter>
         </defs>
       </svg>
-      <div className="gradients-container">
-        <div className="g1"></div>
-        <div className="g2"></div>
-        <div className="g3"></div>
-        <div className="g4"></div>
-        <div className="g5"></div>
-        <div className="interactive"></div>
+      <div className={styles.gradientsContainer}>
+        <div className={styles.g1}></div>
+        <div className={styles.g2}></div>
+        <div className={styles.g3}></div>
+        <div className={styles.g4}></div>
+        <div className={styles.g5}></div>
+        <div ref={interBubbleRef} className={styles.interactive}></div>
       </div>
     </div>
   );
 };
 
-export default GradientBg;
+export default React.memo(GradientBg);
