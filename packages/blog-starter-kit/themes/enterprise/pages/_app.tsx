6cd367942df3189df316d9067f3d072d611132aa
challengingfuture <challengingfuture@gmail.com>
2024-07-05 08:29:56 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
+import '../styles/globals.css';
 import '../styles/OverlaySearch.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
