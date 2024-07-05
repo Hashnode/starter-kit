d2229be6afc9655d17fd3fed4dc7f11d26a16b35
challengingfuture <challengingfuture@gmail.com>
2024-07-05 09:19:17 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
@@ -18,12 +18,6 @@ const GradientBg: React.FC = () => {
 
   return (
     <div className="gradient-bg">
-      <svg className="noiseBg" xmlns="http://www.w3.org/2000/svg">
-        <filter id="noiseFilterBg">
-          <feTurbulence type="fractalNoise" baseFrequency="0.6" stitchTiles="stitch" />
-        </filter>
-        <rect width="100%" height="100%" filter="url(#noiseFilterBg)" />
-      </svg>
       <div className="gradients-container">
         {gradients.map((gradient, index) => (
           <div 
