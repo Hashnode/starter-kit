ef5eb2fb2e40783881af236204b93130874d804e
challengingfuture <challengingfuture@gmail.com>
2024-07-05 09:07:24 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/rainbow/GradientBg.tsx
@@ -1,45 +1,20 @@
-import React, { useEffect, useRef, useCallback } from "react";
+import React, { useMemo } from "react";
+
+const getRandomPosition = () => ({
+  x: Math.random() * 100 - 50,
+  y: Math.random() * 100 - 50
+});
+
+const getRandomDuration = () => Math.random() * 10 + 20; // 20-30 saniye arası
 
 const GradientBg: React.FC = () => {
-  const interBubbleRef = useRef<HTMLDivElement>(null);
-  const animationRef = useRef<number>();
-
-  const move = useCallback(() => {
-    const interBubble = interBubbleRef.current;
-    if (!interBubble) return;
-
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    const animate = () => {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      animationRef.current = requestAnimationFrame(animate);
-    };
-
-    const handleMouseMove = (event: MouseEvent) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-    animate();
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-      if (animationRef.current) {
-        cancelAnimationFrame(animationRef.current);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    const cleanup = move();
-    return cleanup;
-  }, [move]);
+  const gradients = useMemo(() => [
+    { class: 'g1', ...getRandomPosition(), duration: getRandomDuration() },
+    { class: 'g2', ...getRandomPosition(), duration: getRandomDuration() },
+    { class: 'g3', ...getRandomPosition(), duration: getRandomDuration() },
+    { class: 'g4', ...getRandomPosition(), duration: getRandomDuration() },
+    { class: 'g5', ...getRandomPosition(), duration: getRandomDuration() },
+  ], []);
 
   return (
     <div className="gradient-bg">
@@ -49,22 +24,18 @@ const GradientBg: React.FC = () => {
         </filter>
         <rect width="100%" height="100%" filter="url(#noiseFilterBg)" />
       </svg>
-      <svg className="svgBlur" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -10" result="goo" />
-            <feBlend in="SourceGraphic" in2="goo" />
-          </filter>
-        </defs>
-      </svg>
       <div className="gradients-container">
-        <div className="g1"></div>
-        <div className="g2"></div>
-        <div className="g3"></div>
-        <div className="g4"></div>
-        <div className="g5"></div>
-        <div ref={interBubbleRef} className="interactive"></div>
+        {gradients.map((gradient, index) => (
+          <div 
+            key={index}
+            className={`gradient ${gradient.class}`}
+            style={{
+              '--x': `${gradient.x}%`,
+              '--y': `${gradient.y}%`,
+              '--duration': `${gradient.duration}s`,
+            } as React.CSSProperties}
+          />
+        ))}
       </div>
     </div>
   );
