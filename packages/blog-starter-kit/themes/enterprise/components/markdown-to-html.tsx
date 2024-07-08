14687ae975715da4d4ef3f9d112b58aa4a431e07
challengingfuture <challengingfuture@gmail.com>
2024-07-08 13:55:49 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/markdown-to-html.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/markdown-to-html.tsx
@@ -1,22 +1,24 @@
 import { useEmbeds } from '@starter-kit/utils/renderer/hooks/useEmbeds';
 import { markdownToHtml } from '@starter-kit/utils/renderer/markdownToHtml';
-import { memo } from 'react';
+import { memo, useRef } from 'react'; // Import useRef
 
 type Props = {
-	contentMarkdown: string;
+  contentMarkdown: string;
 };
 
 const _MarkdownToHtml = ({ contentMarkdown }: Props) => {
-	const content = markdownToHtml(contentMarkdown);
-	useEmbeds({ enabled: true });
+  const content = markdownToHtml(contentMarkdown);
+  useEmbeds({ enabled: true });
+  // Create a ref to store the reference to the div
+  const contentRef = useRef<HTMLDivElement | null>(null); 
 
-	return (
-		<div
-			className="hashnode-content-style mx-auto w-full px-5 md:max-w-screen-md"
-			dangerouslySetInnerHTML={{ __html: content }}
-			ref={contentRef} 
-		/>
-	);
+  return (
+    <div
+      className="hashnode-content-style mx-auto w-full px-5 md:max-w-screen-md"
+      dangerouslySetInnerHTML={{ __html: content }}
+      ref={contentRef}  // Attach the ref to the div
+    />
+  );
 };
 
 export const MarkdownToHtml = memo(_MarkdownToHtml);
