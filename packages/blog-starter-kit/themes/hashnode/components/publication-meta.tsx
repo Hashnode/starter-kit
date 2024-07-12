ce42d5c23cd8d32ae5db67300159faed775ea385
Shad Mirza <iamshadmirza@gmail.com>
2024-07-12 17:10:04 +0530

    update default image (#161)

--- packages/blog-starter-kit/themes/hashnode/components/publication-meta.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/publication-meta.tsx
@@ -1,35 +1,39 @@
-import { resizeImage } from '../utils/image';
-import { Publication } from '../generated/graphql';
 import { twJoin } from 'tailwind-merge';
+import { Publication } from '../generated/graphql';
+import { resizeImage } from '../utils/image';
 
 // TODO: this component name is confusing.
 const PublicationMeta = (
-  props: Pick<Publication, 'isTeam'> & {
-    author: Pick<Publication['author'], 'name' | 'username' | 'followersCount' | 'profilePicture'>;
-    aboutHTML?: string | null;
-  },
+	props: Pick<Publication, 'isTeam'> & {
+		author: Pick<Publication['author'], 'name' | 'username' | 'followersCount' | 'profilePicture'>;
+		aboutHTML?: string | null;
+	},
 ) => {
-  const { isTeam, aboutHTML, author } = props;
-  const authorImageURL = resizeImage(
-    author.profilePicture || 'https://cdn.hashnode.com/res/hashnode/image/upload/v1659089761812/fsOct5gl6.png',
-    { w: 400, h: 400, c: 'face' },
-  );
+	const { isTeam, aboutHTML, author } = props;
+	const authorImageURL = resizeImage(
+		author.profilePicture ||
+			'https://cdn.hashnode.com/res/hashnode/image/upload/v1720783929907/OSTT0QVJ9.png',
+		{ w: 400, h: 400, c: 'face' },
+	);
 
-  return (
-    <div className="blog-author-card mx-auto px-2 py-8 md:px-8 md:py-10">
-      <div className="flex flex-col flex-wrap items-center">
-        <div className="flex w-full flex-col items-center">
-          {aboutHTML ? (
-            <div
-              className={twJoin('prose text-center dark:prose-dark', isTeam ? 'max-w-full lg:prose-xl' : '')}
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html: aboutHTML }}
-            />
-          ) : null}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="blog-author-card mx-auto px-2 py-8 md:px-8 md:py-10">
+			<div className="flex flex-col flex-wrap items-center">
+				<div className="flex w-full flex-col items-center">
+					{aboutHTML ? (
+						<div
+							className={twJoin(
+								'prose dark:prose-dark text-center',
+								isTeam ? 'lg:prose-xl max-w-full' : '',
+							)}
+							// eslint-disable-next-line react/no-danger
+							dangerouslySetInnerHTML={{ __html: aboutHTML }}
+						/>
+					) : null}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PublicationMeta;
--- packages/blog-starter-kit/themes/hashnode/utils/const/index.ts
+++ packages/blog-starter-kit/themes/hashnode/utils/const/index.ts
@@ -1,15 +1,14 @@
-import { getAppUrl } from "../urls";
+import { getAppUrl } from '../urls';
 
 export const DEFAULT_AVATAR =
-	'https://cdn.hashnode.com/res/hashnode/image/upload/v1659089761812/fsOct5gl6.png';
+	'https://cdn.hashnode.com/res/hashnode/image/upload/v1720783929907/OSTT0QVJ9.png';
 
 export const MAX_MAIN_NAV_LINKS = 7;
 
 export const DEFAULT_COVER =
 	'https://cdn.hashnode.com/res/hashnode/image/upload/v1683525272978/MB5H_kgOC.png?auto=format';
 
-
 export const DEFAULT_LIGHT_POST_COVER =
-  'https://cdn.hashnode.com/res/hashnode/image/upload/v1683525272978/MB5H_kgOC.png?auto=format';
+	'https://cdn.hashnode.com/res/hashnode/image/upload/v1683525272978/MB5H_kgOC.png?auto=format';
 
 export const HASHNODE_NEXT_URL = getAppUrl();
