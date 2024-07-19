7585dc7af2c1beab069502d0f15276ddcdde9324
challengingfuture <challengingfuture@gmail.com>
2024-07-19 14:35:36 +0000

    buton rengi değiştirildi arama - daha fazla ara

--- packages/blog-starter-kit/themes/enterprise/components/searchbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/searchbar.tsx
@@ -208,7 +208,7 @@ export const Search: React.FC<SearchProps> = ({ isOpen, onClose }) => {
 				  {hasNextPage && (
 					<button
 					  onClick={loadMore}
-					  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 w-full"
+					  className="mt-4 px-4 py-2 bouncing-blob--purple text-white rounded hover:bg-orng-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 w-full"
 					  disabled={isSearching}
 					>
 					  {isSearching ? 'Yükleniyor...' : 'Daha fazla yükle'}
