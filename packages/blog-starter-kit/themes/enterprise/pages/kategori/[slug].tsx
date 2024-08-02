f5b73159fd8a2dea5c3371fde92c88a667848908
challengingfuture <challengingfuture@gmail.com>
2024-08-02 11:50:55 +0000

    feat(kategori/[slug]): remove unused imports and components - Removed unused imports for `resizeImage`, `CoverImage`, and `Header` components - Removed commented-out code block that was rendering the series cover image and description

--- packages/blog-starter-kit/themes/enterprise/pages/kategori/[slug].tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/kategori/[slug].tsx
@@ -1,12 +1,9 @@
-import { resizeImage } from '@starter-kit/utils/image';
 import request from 'graphql-request';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { Container } from '../../components/container';
 import { AppProvider } from '../../components/contexts/appContext';
-import { CoverImage } from '../../components/cover-image';
 import { Footer } from '../../components/footer';
-import { Header } from '../../components/header';
 import { Layout } from '../../components/layout';
 import { Navbar } from "../../components/navbar";
 
@@ -19,7 +16,6 @@ import {
 	SeriesPostsByPublicationQuery,
 	SeriesPostsByPublicationQueryVariables,
 } from '../../generated/graphql';
-import { DEFAULT_COVER } from '../../utils/const';
 
 type Props = {
 	series: SeriesFragment;
@@ -40,36 +36,6 @@ export default function Post({ series, publication, posts }: Props) {
 				</Head>
 				<Navbar />
 				<Container className="flex flex-col items-stretch gap-10 px-5 pb-10">
-					{/* <div
-						className={`${
-							series.coverImage ? 'col-span-full' : 'col-span-3'
-						} grid grid-cols-4 pt-5 md:gap-5`}
-					>
-						<div className="col-span-full flex flex-col gap-1 md:col-span-2 lg:col-span-3">
-							<p className="font-bold uppercase text-slate-500 dark:text-neutral-400">Series</p>
-							<h1 className="text-4xl font-bold text-slate-900 dark:text-neutral-50">
-								{series.name}
-							</h1>
-							<div
-								className="hashnode-content-style"
-								dangerouslySetInnerHTML={{ __html: series.description?.html ?? '' }}
-							></div>
-						</div>
-						<div className="relative col-span-full md:col-span-2 lg:col-span-1">
-							<CoverImage
-								title={series.name}
-								src={resizeImage(
-									series.coverImage,
-									{
-										w: 400,
-										h: 210,
-										c: 'thumb',
-									},
-									DEFAULT_COVER,
-								)}
-							/>
-						</div>
-					</div> */}
 					{posts.length > 0 ? (
 						<MorePosts context="series" posts={posts} />
 					) : (
