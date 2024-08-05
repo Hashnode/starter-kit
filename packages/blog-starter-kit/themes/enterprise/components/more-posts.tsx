6d2e3498b6f9cbb9b7adf596f791ac3d55512242
challengingfuture <challengingfuture@gmail.com>
2024-08-05 13:23:23 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
@@ -10,12 +10,12 @@ export const MorePosts = ({ posts, context }: Props) => {
 	return (
 		<section className="mb-10 flex flex-col items-start gap-10">
 			{context === 'home' && (
-			  <div className="py-20 text-center flex flex-col items-center">
-							  <p className="text-md leading-snug text-slate-500 dark:text-neutral-400 text-lg max-w-xl mx-auto mt-4">
-			  Kedilerle ve köpeklerle ilgili öğrenmek istediğiniz başka bir şey varsa önceki yazılarımıza göz atabilirsiniz.
-			  </p>
-			  <img loading="lazy" className="w-1/2 block" alt="Sayfa ayracı" src="https://9kelt5xnesj2nkgz.public.blob.vercel-storage.com/underline-G32Fu5noQ2QvXU7bIiaq9zclJsLPcm.png" />
-			</div>
+			  <div className="hidden md:flex py-20 text-center flex-col items-center">
+				<p className="text-md leading-snug text-slate-500 dark:text-neutral-400 text-lg max-w-xl mx-auto mt-4">
+				  Kedilerle ve köpeklerle ilgili öğrenmek istediğiniz başka bir şey varsa önceki yazılarımıza göz atabilirsiniz.
+				</p>
+				<img loading="lazy" className="w-1/2 block" alt="Sayfa ayracı" src="https://9kelt5xnesj2nkgz.public.blob.vercel-storage.com/underline-G32Fu5noQ2QvXU7bIiaq9zclJsLPcm.png" />
+			  </div>
 			)}
 			<div className="grid items-start gap-5 md:grid-cols-2 xl:grid-cols-3">
 				{posts.map((post) => (
