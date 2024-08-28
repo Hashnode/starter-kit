d8db0acbdb5e4dc3a87065bae3a18861ad2550b3
challengingfuture <challengingfuture@gmail.com>
2024-08-28 08:19:28 +0000

    Revert "."
    This reverts commit 32de0a9dcceedffddea6873d31aa69523a13668f.

--- packages/blog-starter-kit/themes/enterprise/components/SafeLink.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/SafeLink.tsx
@@ -1,29 +1,30 @@
+// src/components/SafeLink.tsx
 import React from 'react';
-import Link, { LinkProps } from 'next/link';
+import Link from 'next/link';
 import { useExternalLink } from '../components/contexts/ExternalLinkContext';
 
-type SafeLinkProps = Omit<LinkProps, 'href'> & {
-  href: string;
-  children: React.ReactNode;
-};
-
-const INTERNAL_DOMAINS = ['blog.temizmama.com', 'www.temizmama.com'];
-
-export const SafeLink: React.FC<SafeLinkProps> = ({ href, children, ...props }) => {
-  const { showModal } = useExternalLink();
-
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    const isExternal = !INTERNAL_DOMAINS.some(domain => href.includes(domain));
-
-    if (isExternal) {
-      e.preventDefault();
-      showModal(href);
-    }
+type SafeLinkProps = React.ComponentProps<typeof Link> & {
+    children: React.ReactNode;
   };
-
-  return (
-    <Link href={href} {...props} onClick={handleClick}>
-      {children}
-    </Link>
-  );
-};
+  
+  const INTERNAL_DOMAINS = ['blog.temizmama.com', 'www.temizmama.com'];
+  
+  export const SafeLink: React.FC<SafeLinkProps> = ({ href, children, ...props }) => {
+    const { showModal } = useExternalLink();
+  
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      const url = href.toString();
+      const isExternal = !INTERNAL_DOMAINS.some(domain => url.includes(domain));
+  
+      if (isExternal) {
+        e.preventDefault();
+        showModal(url);
+      }
+    };
+  
+    return (
+      <Link href={href} onClick={handleClick} {...props}>
+        {children}
+      </Link>
+    );
+  };
