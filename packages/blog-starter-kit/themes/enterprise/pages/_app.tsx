ef69ebce0e81e8f13059a2ce489d4191a3fab2b0
challengingfuture <challengingfuture@gmail.com>
2024-08-28 09:21:36 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
@@ -3,11 +3,9 @@ import { AppProps } from 'next/app';
 import { AppProvider } from '../components/contexts/appContext';
 import { useExternalLinkHandler } from '../utils/externalLinkHandler';
 import { ExternalLinkModal } from '../components/ExternalLinkModal';
-import { ThemeSwitcher } from '../components/ThemeSwitcher';
 import '../styles/index.css';
 import '../styles/globals.css';
 import '../styles/OverlaySearch.css';
-import '../styles/themes.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { modalUrl, hideModal } = useExternalLinkHandler();
@@ -25,11 +23,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       page={pageProps.page}
       series={pageProps.series}
     >
-      <div className="app-container">
-        <ThemeSwitcher />
-        <Component {...pageProps} />
-        {modalUrl && <ExternalLinkModal url={modalUrl} onClose={hideModal} />}
-      </div>
+      <Component {...pageProps} />
+      {modalUrl && <ExternalLinkModal url={modalUrl} onClose={hideModal} />}
     </AppProvider>
   );
 }
