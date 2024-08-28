32de0a9dcceedffddea6873d31aa69523a13668f
challengingfuture <challengingfuture@gmail.com>
2024-08-28 07:53:33 +0000

    .

--- packages/blog-starter-kit/themes/enterprise/components/SafeLink.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/SafeLink.tsx
@@ -1,30 +1,29 @@
-// src/components/SafeLink.tsx
 import React from 'react';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { useExternalLink } from '../components/contexts/ExternalLinkContext';
 
-type SafeLinkProps = React.ComponentProps<typeof Link> & {
-    children: React.ReactNode;
+type SafeLinkProps = Omit<LinkProps, 'href'> & {
+  href: string;
+  children: React.ReactNode;
+};
+
+const INTERNAL_DOMAINS = ['blog.temizmama.com', 'www.temizmama.com'];
+
+export const SafeLink: React.FC<SafeLinkProps> = ({ href, children, ...props }) => {
+  const { showModal } = useExternalLink();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const isExternal = !INTERNAL_DOMAINS.some(domain => href.includes(domain));
+
+    if (isExternal) {
+      e.preventDefault();
+      showModal(href);
+    }
   };
-  
-  const INTERNAL_DOMAINS = ['blog.temizmama.com', 'www.temizmama.com'];
-  
-  export const SafeLink: React.FC<SafeLinkProps> = ({ href, children, ...props }) => {
-    const { showModal } = useExternalLink();
-  
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      const url = href.toString();
-      const isExternal = !INTERNAL_DOMAINS.some(domain => url.includes(domain));
-  
-      if (isExternal) {
-        e.preventDefault();
-        showModal(url);
-      }
-    };
-  
-    return (
-      <Link href={href} onClick={handleClick} {...props}>
-        {children}
-      </Link>
-    );
-  };
+
+  return (
+    <Link href={href} {...props} onClick={handleClick}>
+      {children}
+    </Link>
+  );
+};
