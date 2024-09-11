338b9fc1e02e865006223ac2e99f283864cb7d95
Bigo <mesutb@temizmama.com>
2024-09-11 18:07:43 +0300

    kopek içinde eklendi

---
+++ packages/blog-starter-kit/themes/enterprise/pages/kopek/sayfa/[sayfa].tsx
@@ -0,0 +1,27 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
+import KopekPage, { getStaticProps as kopekGetStaticProps } from '../../kedi';
+
+export default KopekPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const kopekProps = await kopekGetStaticProps(context);
+  
+  if ('props' in kopekProps) {
+    return {
+      ...kopekProps,
+      props: {
+        ...kopekProps.props,
+        currentPage: context.params?.sayfa ? parseInt(context.params.sayfa as string) : 1,
+      },
+    };
+  }
+  
+  return kopekProps;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { sayfa: '1' } }, { params: { sayfa: '2' } }],
+    fallback: 'blocking',
+  };
+};
