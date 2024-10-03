ecca05d839707c559de29c8c3fa370ecc7e1e7e9
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2024-10-04 00:24:32 +0530

    Revert "Fix js error in enterprise theme when no publication links are config…"
    This reverts commit 2643a7ada13f4840725d6cfaed5ff83f5bfcfa86.

--- packages/blog-starter-kit/themes/enterprise/components/social-links.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/social-links.tsx
@@ -4,9 +4,8 @@ import { GithubSVG, HashnodeSVG, LinkedinSVG, RssSVG, XSVG } from './icons';
 
 export const SocialLinks = ({ isSidebar }: { isSidebar?: boolean }) => {
 	const { publication } = useAppContext();
-	const hasSocialLinks = publication?.links
-		? !Object.values(publication.links!).every((val) => val === '')
-		: false;
+	const hasSocialLinks = !Object.values(publication.links!).every((val) => val === '');
+
 	return (
 		<>
 			<div
