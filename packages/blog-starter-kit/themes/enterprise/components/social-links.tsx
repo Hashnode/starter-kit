05e334c17e0292da796b61a8fd0347262afbd6e6
Harsh <harsh@hashnode.com>
2024-10-03 16:23:38 +0530

    Fix js error in enterprise theme when no publication links are configured

--- packages/blog-starter-kit/themes/enterprise/components/social-links.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/social-links.tsx
@@ -4,8 +4,9 @@ import { GithubSVG, HashnodeSVG, LinkedinSVG, RssSVG, XSVG } from './icons';
 
 export const SocialLinks = ({ isSidebar }: { isSidebar?: boolean }) => {
 	const { publication } = useAppContext();
-	const hasSocialLinks = !Object.values(publication.links!).every((val) => val === '');
-
+	const hasSocialLinks = publication?.links
+		? !Object.values(publication.links!).every((val) => val === '')
+		: false;
 	return (
 		<>
 			<div
